3ab7cbeebc995cd10b45a1b6154ad0fe570f544c
Zackrey Bichsel <biksel09@gmail.com>
2024-09-06 16:38:09 -0500

    initial commit

---
+++ src/main/java/com/technews/tech_news_java/TechNewsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.technews.tech_news_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNewsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNewsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technews/tech_news_java/TechNewsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technews.tech_news_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNewsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
